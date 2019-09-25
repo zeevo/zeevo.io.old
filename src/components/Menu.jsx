b8acf41fefdc3acb33747c819977423b7f989e63
zeevosec <8338013+zeevosec@users.noreply.github.com>
2019-09-24 22:33:04 -0400

    feat: add projects page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,31 +10,15 @@ class Menu extends React.Component {
       <ul className="menu__list">
         {menu.map(item => {
           let link;
-          console.log(item);
-          if (item.label === 'Projects') {
-            link = 'Projects (Soon)';
-          } else if (item.path.startsWith('http')) {
-            link = (
-              <a
-                href={item.path}
-                rel="noopener noreferrer"
-                target="_blank"
-                className="menu__list-item-link"
-              >
-                {item.label}
-              </a>
-            );
-          } else {
-            link = (
-              <Link
-                to={item.path}
-                className="menu__list-item-link"
-                activeClassName="menu__list-item-link menu__list-item-link--active"
-              >
-                {item.label}
-              </Link>
-            );
-          }
+          link = (
+            <Link
+              to={item.path}
+              className="menu__list-item-link"
+              activeClassName="menu__list-item-link menu__list-item-link--active"
+            >
+              {item.label}
+            </Link>
+          );
           return (
             <li className="menu__list-item" key={item.path}>
               {link}
