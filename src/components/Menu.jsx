18d7cf8eb4cb80f828625926e4d243ab4717cbc8
zeevosec <8338013+zeevosec@users.noreply.github.com>
2019-09-28 15:31:48 -0400

    feat: change them and fix broken links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import './style/menu.scss';
 
+const isExternal = path => path.startsWith('http');
 class Menu extends React.Component {
   render() {
     const menu = this.props.data;
@@ -9,16 +10,30 @@ class Menu extends React.Component {
     const menuBlock = (
       <ul className="menu__list">
         {menu.map(item => {
+          console.log(item);
           let link;
-          link = (
-            <Link
-              to={item.path}
-              className="menu__list-item-link"
-              activeClassName="menu__list-item-link menu__list-item-link--active"
-            >
-              {item.label}
-            </Link>
-          );
+          if (isExternal(item.path)) {
+            link = (
+              <a
+                className="menu__list-item-link"
+                rel="noopener norefferer"
+                target="_blank"
+                href={item.path}
+              >
+                {item.label}
+              </a>
+            );
+          } else {
+            link = (
+              <Link
+                to={item.path}
+                className="menu__list-item-link"
+                activeClassName="menu__list-item-link menu__list-item-link--active"
+              >
+                {item.label}
+              </Link>
+            );
+          }
           return (
             <li className="menu__list-item" key={item.path}>
               {link}
