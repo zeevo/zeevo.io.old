d865f662df984b06d3541fbc58db994b4c2709a5
zeevosec <8338013+zeevosec@users.noreply.github.com>
2020-08-09 21:41:12 -0400

    feat: change PWA color

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,7 +152,7 @@ module.exports = {
         short_name: siteConfig.title,
         start_url: '/',
         background_color: '#FFF',
-        theme_color: '#F7A046',
+        theme_color: '#0E0E0E',
         display: 'standalone',
         icon: 'static/photo.png',
       },
